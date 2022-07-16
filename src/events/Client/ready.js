9b3930f5ab1cf04156a64a79a2b6512fe16a2474
YELLOW <98357285+YELLOWWWWWWW@users.noreply.github.com>
2022-07-16 10:08:24 -0700

    Update ready.js

--- src/events/Client/ready.js
+++ src/events/Client/ready.js
@@ -8,7 +8,7 @@ run: async (client) => {
     client.logger.log(`Ready on ${client.guilds.cache.size} servers, for a total of ${client.guilds.cache.reduce((a, g) => a + g.memberCount, 0)} users`, "ready");
 
     //Game
-    let statuses = ['Wade Server'`];
+    let statuses = ['Wade Server'];
     setInterval(function() {
   		let status = statuses[Math.floor(Math.random()*statuses.length)];
   		client.user.setActivity(status, {type: "WATCHING"});
